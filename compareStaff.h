f72b33cb3c0d4060092a52ce4cdf0e418c44e6e6
Boyi <2809984835@qq.com>
2019-07-17 03:27:07 +0000

    Updated compareStaff.h-重命名函数名

--- compareStaff.h
+++ compareStaff.h
@@ -7,13 +7,13 @@ extern vector<Officialstaff> oStaffList;
 extern vector<Tempstaff> tStaffList;
 
 //根据正式员工编号升序排序
-bool comp_num(const Officialstaff& s0, const Officialstaff& s1);
+bool comp_num_up(const Officialstaff& s0, const Officialstaff& s1);
 
 //重载，根据临时员工编号升序排序
-bool comp_num(const Tempstaff& t0, const Tempstaff& t1);
+bool comp_num_up(const Tempstaff& t0, const Tempstaff& t1);
 
 //根据正式员工实发工资升序排序，若相同，则按编号升序排序
-bool comp_salary(const Officialstaff& s0, const Officialstaff& s1);
+bool comp_salary_up(const Officialstaff& s0, const Officialstaff& s1);
 
 //重载，根据临时员工实发工资升序排序
-bool comp_salary(const Tempstaff& t0, const Tempstaff& t1);
+bool comp_salary_up(const Tempstaff& t0, const Tempstaff& t1);
